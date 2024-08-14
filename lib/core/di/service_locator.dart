8939e1cdfa9372f503b3eed1a52d78413a8a2637
abdo1917 <102588811+abdo1917@users.noreply.github.com>
2024-08-14 15:11:37 +0300

    add some extensions

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,13 +1,31 @@
 import 'package:get_it/get_it.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
+import '../../data/repository/favorite_repository.dart';
+import '../../data/repository/player_repository.dart';
+import '../../data/repository/recent_repository.dart';
+import '../../data/repository/search_repository.dart';
 import '../../data/repository/song_repository.dart';
 import '../../data/repository/theme_repository.dart';
 
 final sl = GetIt.instance;
-void init(){
 
+void init() {
   // Repository
-  sl.registerLazySingleton(() => ThemeRepository(),);
-  sl.registerLazySingleton(() => SongRepository(),);
+  sl.registerLazySingleton(
+    () => ThemeRepository(),
+  );
+  sl.registerLazySingleton(
+    () => SongRepository(),
+  );
+  sl.registerLazySingleton<MusicPlayer>(
+    () => JustAudioPlayer(),
+  );
+  sl.registerLazySingleton(() => SongRepository());
+  sl.registerLazySingleton(() => FavoritesRepository());
+  sl.registerLazySingleton(() => RecentRepository());
+  sl.registerLazySingleton(() => SearchRepository());
 
+  // Third Party
+  sl.registerLazySingleton(() => OnAudioQuery());
 }
